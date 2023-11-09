fed287cccedc89b96a9392d78a891dd1214af117
rchmtmaulana <rachmatmaulana81669@gmail.com>
2023-11-09 17:53:27 +0800

    feat: tambah fitur ganti nama + bio

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,21 @@
+'use client'
+import { useState } from 'react'
+
 import Image from 'next/image'
 import './globals.css'
 
 export default function Home() {
+
+  const [duit, setDuit] = useState(0)
+  const [nama, setNama] = useState('Rachmat Maulana')
+
+  function handlerTambahDuit() {
+    setDuit(duit + 1)
+  }
+  function handlerGantiNama() {
+    setNama('Teknik Informatika 21')
+  }
+
   return (
     <div className='body'>
       <div className="banner-container">
@@ -19,21 +33,22 @@ export default function Home() {
           </div>
           <div className="content-header-banner">
             {/* nama dan kawan2 */}
-            <h1>Rachmat Maulana</h1>
+            <h1>{nama}</h1>
             <div className="bio-nim-header-banner">
             {/* NIM dan BIO */}
             <p>D121211015</p>
-            <p>Do it to get duit</p>
+            <p>Do it to get {duit}</p>
           </div>
           </div>
         </div>
         <div className="cta-banner-wrapper">
           {/* tombol cta */}
-          <a href=''>
-            <div className='cta-button'>
+            <div className='cta-button' onClick={handlerTambahDuit}>
               <p>Halo!!!</p>
             </div>
-          </a>
+            <div className='cta-button' style={{marginTop: '12px'}} onClick={handlerGantiNama}>
+              <p>GantiNama</p>
+            </div>
         </div>
       </div>
     </div>
